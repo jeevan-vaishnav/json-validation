888fbbcfa89c0c3cff3986689f66e1ef2aa2f7d4
Jeevan Vaishnav <jeevanvaishnavv@gmail.com>
2024-10-18 21:44:21 +0530

    final commit

--- server.js
+++ server.js
@@ -9,6 +9,17 @@ const invalidDir = path.join(__dirname, "error-json");
 //call the watcher function
 watcherJSON(sourceDir, validDir, invalidDir);
 
+//Function to process and validate a single JSON FILE
+
+async function processJsonFile(filePath, validDir, errorDir) {
+  try {
+    const fileName = path.basename(filePath);
+    //read file content
+    const fileData = await fsPromises.readFile(filePath, "utf-");
+    console.log(fileData);
+  } catch (error) {}
+}
+
 async function watcherJSON(sourceDir, validDir, invalidDir) {
   console.time("server");
   //Ensure the valid and error directorires exits
@@ -24,21 +35,62 @@ async function watcherJSON(sourceDir, validDir, invalidDir) {
   for await (const event of watcher) {
     console.log(event);
     if (event.eventType === "rename") {
-      const filePath = path.join(sourceDir, event.filename);
+      const filePath = path.join(sourceDir, event?.filename);
       console.log(`FilePath: ${filePath}`);
       //check if the file still exits
-
       try {
         const fileStat = await fsPromises.stat(filePath);
-        console.log(`FileStat: ${fileStat.size}`);
-        if (fileStat.isFile()) {
+        console.log(`FileStat: ${fileStat?.size}`);
+        if (fileStat?.isFile()) {
           console.log(`New File detected: ${event.filename}`);
+          ( async () => {
+
+            const fileName = path.basename(filePath);
+            // const oldWay = await fsPromises.readFile(filePath,'utf-8');
+            // console.log("OldWay:" + oldWay);
+            const fileData = await fsPromises.open(filePath, "r");
+            const readStream = fileData.createReadStream({ encoding: "utf-8" });
+            let data = "";
+            readStream.on("data", (chunk) => {
+              data += chunk; // accumulate chunks of data
+            });
+            readStream.on("end", async () => {              
+              //ValidDate the JSON
+              if(isValidJSON(data)){
+                //Move to valid directory
+                await fsPromises.rename(filePath,path.join(validDir,fileName));
+                console.log(`Valid JSON: ${fileName}`);
+              }else{
+                // Move to error directory
+                await fsPromises.rename(filePath, path.join(invalidDir, fileName));
+                console.log(`Invalid JSON: ${fileName}`);              }
+            });
+          })();
         }
       } catch (err) {
-        console.error(`Error detecting file: ${err.message}`);
+        // console.error(`Error detecting file: ${err.message}`);
       }
     }
   }
 
   console.timeEnd("server");
+}
+
+
+function isValidJSON(jsonData) {
+  try {
+    const data = JSON.parse(jsonData);
+    //validate required fields:id,name,age
+    if(!data.id || !data.name || !data.age){
+        return false
+    }
+
+    //age should be number
+    if(isNaN(Number(data.age))){
+      return false
+    }
+    return true;
+  } catch (error) {
+    return false;
+  }
 }
